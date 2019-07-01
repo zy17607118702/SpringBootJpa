030433298b85358d692ba484e9d08d505c75ae30
zy17607118702 <31174349+zy17607118702@users.noreply.github.com>
2019-07-01 17:31:16 +0800

    代码提交

--- src/main/java/com/cn/springbootjpa/master/controller/TmSysUserApiController.java
+++ src/main/java/com/cn/springbootjpa/master/controller/TmSysUserApiController.java
@@ -4,11 +4,13 @@
 package com.cn.springbootjpa.master.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cn.springbootjpa.base.bo.BaseBo;
 import com.cn.springbootjpa.base.controller.BaseController;
+import com.cn.springbootjpa.base.exception.AppException;
 import com.cn.springbootjpa.master.bo.TmSysUserBo;
 import com.cn.springbootjpa.master.entity.TmSysUser;
 import com.cn.springbootjpa.util.MD5;
@@ -37,9 +39,9 @@ public class TmSysUserApiController extends BaseController<TmSysUser, Integer> {
 		//用户加密
 		model.setUserPwd(MD5.md5(model.getUserPwd()));
 	}
-
-	@Override
-	public void afterGetById(TmSysUser t) {}
 	
-	
+	@GetMapping(value="exception")
+	public void ExceptionTest() {
+		throw new AppException("测试异常类");
+	}
 }
